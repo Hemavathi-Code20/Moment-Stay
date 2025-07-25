7628fe85182d6584f9ce3e669247ba87d26366f1
Hemavathi-Code20 <hemavathisiva05@gmail.com>
2025-07-25 15:52:17 +0530

    commit

--- client/src/components/ListingDetails.jsx
+++ client/src/components/ListingDetails.jsx
@@ -253,7 +253,7 @@ const ListingDetails = () => {
                   handleSubmit();
                 }}
               >
-                Book Now
+                Book
               </button>
 
             </div>
